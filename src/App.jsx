272f797c7ba0c3849662bdd8dad9eb56152f8396
ala <ala.hammami@sesame.com.tn>
2024-03-11 10:35:33 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import * as React from "react" 
+function App() {
+  const title ="React"; 
+  return (
+    <div>
+      <h1>Hello {title}</h1>
+    </div>
+  );
+}
+
+export default App
